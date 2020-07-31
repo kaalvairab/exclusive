19b1160c852b08d3fd09685a93cf203c1621b8ad
Kaalvairab <itskaalvairab@gmail.com>
2020-07-31 15:58:13 +0545

    landing page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; {
+  for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
-    if(file) {
+    if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function () {
